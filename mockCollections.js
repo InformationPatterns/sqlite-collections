f5781a473c8fb69defe01dae6bb91f435bf8f899
Sivli Kestanous <cmtoups@me.com>
2016-03-23 09:05:55 -0400

    speed up uploads! Like by 100x

--- mockCollections.js
+++ mockCollections.js
@@ -13,27 +13,21 @@ SQLite.Collection = class SQLiteCollection extends Mongo.Collection {
     }
     if (Meteor.server) {
       var self = this;
+      self.batchInsert = {}
       methods = {}
       methods[`/${name}/batchInsert`] = function (docs) {
         if (!docs || !docs.length) { return 0; }
-        var userId = this.userId;
-
+        var userId = this.userId
+        , user = Meteor.users.findOne(userId)
         docs.forEach(function (doc) {
-          // call user validators.
-          // Any deny returns true means denied.
-          if (_.any(self._validators.insert.deny, function(validator) {
-            return validator(userId, doc);
-          })) {
-            throw new Meteor.Error(403, "Access denied");
-          }
           // Any allow returns true means proceed. Throw error if they all fail.
-          if (_.all(self._validators.insert.allow, function(validator) {
-            return !validator(userId, doc);
-          })) {
-            throw new Meteor.Error(403, "Access denied");
-          }
-          try { self.insert(doc); } catch (e) {} //there may be cases of duplicate _ids, just move on
-        });    
+          if (self.batchInsert.allow) {
+            if (!self.batchInsert.allow(user, doc)) {
+              throw new Meteor.Error(403, "Access denied");
+            }
+          } 
+          try { self.insert(doc); } catch (e) {} //there may be cases of duplicate _ids, drop and just move on
+        });
         return docs.length;
       }
       Meteor.methods(methods);
